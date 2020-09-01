3bf805eb502f359a3fe47643f743682b8126aaad
Craig Nockels <cnockels@gmail.com>
2020-08-31 23:55:39 -0400

    start on searchbar

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -1,12 +1,29 @@
 import React from 'react';
 import styled from 'styled-components';
 
+import SearchBar from '../Searchbar';
+import FilmsDisplay from '../FilmsDisplay';
+import NominationsDisplay from '../NominationDisplay';
+
 const Main = () => {
   return (
-    <div>
+    <StyledDiv>
       Nomination Station
-    </div>
+      <SearchBar/>
+        <Content>
+          <FilmsDisplay/>
+          <NominationsDisplay/>
+        </Content>
+    </StyledDiv>
   );
 }
+
+const StyledDiv = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+const Content = styled.div`
+  display: flex;
+`;
 
 export default Main;
--- src/components/Searchbar/SearchBar.tsx
+++ src/components/Searchbar/SearchBar.tsx
@@ -1,19 +1,58 @@
-import React from 'react'; 
+import React, {useState} from 'react'; 
 import styled from 'styled-components'; 
 
 const SearchBar = () => { 
+  const [inputValue, setInputValue] = useState<string>('');
+  const [disable, setDisable] = useState<boolean>(false);
+
+  const submitHandle = (inputValue: string | null) : void => {
+    console.log('inputValue', inputValue);
+  };
 
   return (
-    <StyledDiv> 
+    <StyledForm> 
       <div> SearchBar </div>
-    </StyledDiv> 
+      <button
+          type='submit'
+          onClick={(e)=>{
+            e.preventDefault();
+            submitHandle(null);
+          }}
+          disabled={disable}
+        >
+          Search!
+        </button>
+        <SearchBox>
+          <StyledInput 
+            type="text"
+            onChange={(e)=>setInputValue(e.target.value)}
+            value={inputValue}
+            placeholder={'Enter Film Title'}
+          />
+          <ClearButton
+            onClick={()=>setInputValue('')}
+          >
+            X
+          </ClearButton>
+        </SearchBox>
+    </StyledForm> 
   ) 
 }; 
 
 
-export default SearchBar;
-
-
-const StyledDiv = styled.div`
+const StyledForm = styled.form`
 
 `;
+
+const SearchBox = styled.div`
+  
+`;
+const StyledInput = styled.input`
+  
+`;
+const ClearButton = styled.div`
+
+`;
+
+
+export default SearchBar;
