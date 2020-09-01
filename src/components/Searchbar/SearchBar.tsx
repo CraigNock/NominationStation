f6a3be765be348050a413f59dfad25ae23e8fd56
Craig Nockels <cnockels@gmail.com>
2020-09-01 19:54:48 -0400

    sort clear button positioning

--- src/components/Searchbar/SearchBar.tsx
+++ src/components/Searchbar/SearchBar.tsx
@@ -15,60 +15,79 @@ const SearchBar: React.FC<props> = ({setSearchResults}) => {
 
   const submitHandle = async (input: string | null) : Promise<any> => {
     if(!input) return;
+  //disables search button until results fetched
+    setDisable(true);
     let results: string[] = await getFilms(input);
     console.log('submitSearch results', results);
     setSearchResults(results);
+    setDisable(false);
   };
 
   return (
     <StyledForm> 
-      <div> SearchBar </div>
-      <button
-          type='submit'
-          onClick={(e)=>{
-            e.preventDefault();
-            if(inputValue === '') return;
-            // setDisable(true);
-            submitHandle(inputValue);
-          }}
-          disabled={disable}
+      <SearchBox>
+        <StyledInput 
+          type="text"
+          onChange={(e)=>setInputValue(e.target.value)}
+          value={inputValue}
+          placeholder={'Enter Film Title'}
+        />
+        <ClearButton
+          onClick={()=>setInputValue('')}
         >
-          Search!
-        </button>
-        <SearchBox>
-          <StyledInput 
-            type="text"
-            onChange={(e)=>setInputValue(e.target.value)}
-            value={inputValue}
-            placeholder={'Enter Film Title'}
-          />
-          <ClearButton
-            onClick={()=>setInputValue('')}
-          >
-            X
-          </ClearButton>
-        </SearchBox>
+          X
+        </ClearButton>
+      </SearchBox>
+      <SearchButton
+        type='submit'
+        onClick={(e)=>{
+          e.preventDefault();
+          if(inputValue === '') return;
+          submitHandle(inputValue);
+        }}
+        disabled={disable}
+      >
+        Search!
+      </SearchButton>
     </StyledForm> 
   ) 
 }; 
 
 
 const StyledForm = styled.form`
+  display: flex;
   margin: 1rem;
 `;
 
 const SearchBox = styled.div`
-  
+  position: relative;
+  display: flex;
+  align-items: center;
+  height: 1.5rem;
+  width: fit-content;
+  /* border: 1px solid red; */
 `;
 const StyledInput = styled.input`
-  
+  height: 1.1rem;
 `;
 const ClearButton = styled.div`
   display: inline-block;
-  background: gray;
+  position: absolute;
+  height: 1rem;
+  width: 1rem;
+  top: .25rem;
+  right: .25rem;
+  text-align: center;
+  font-family: 'Orbitron', sans-serif;
+  font-weight: bold;
+  background: lightgray;
+  border-radius: 20%;
   &:hover {
     cursor: pointer;
   }
+`;
+const SearchButton = styled.button`
+
 `;
 
 
